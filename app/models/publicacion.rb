5b8aa9f2e77f0f9248eec64ee6cef6e6991318f6
JorgeAndaur <aenimalabs@gmail.com>
2021-12-05 21:59:10 -0300

    eliminar en cascada

--- app/models/publicacion.rb
+++ app/models/publicacion.rb
@@ -1,5 +1,5 @@
 class Publicacion < ApplicationRecord
-  has_many :mensajes
+  has_many :mensajes, dependent: :destroy
   belongs_to :blog1
   validates :titulo, :contenido, presence: true
   validates :titulo, length: {minimum: 7}
